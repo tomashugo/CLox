06599667e28542536e463ffd75342fe35f2e7578
Tomás Almeida <tomas.hugo@gmail.com>
2022-04-06 22:07:11 -0300

    feat: Implementado a instrução OP_CONSTANT

--- CLox/value.h
+++ CLox/value.h
@@ -14,6 +14,7 @@ typedef struct {
 void initValueArray(ValueArray* array);
 void writeValueArray(ValueArray* array, Value value);
 void freeValueArray(ValueArray* array);
+void printValue(Value value);
 
 #endif // !clox_value_h
 
