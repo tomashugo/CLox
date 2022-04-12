2c0e3045463634a8d29ce7ddb7933498a3fe871d
Tomás Almeida <tomas.hugo@gmail.com>
2022-04-11 21:04:38 -0300

    feat: Diferenciando tipos internamente

--- CLox/vm.c
+++ CLox/vm.c
@@ -8,6 +8,23 @@
 
 VM vm;
 
+void resetStack() {
+
+}
+
+static void runtimeError(const char* format, ...) {
+	va_list args;
+	va_start(args, format);
+	vprintf(stderr, format, args);
+	va_end(args);
+	fputs("\n", stderr);
+
+	size_t instruction = vm.ip - vm.chunk->code - 1;
+	int line = vm.chunk->lines[instruction];
+	fprintf(stderr, "[line %d] in script\n", line);
+	resetStack();
+}
+
 void initVM() {
 	vm.stackTop = vm.stack;
 }
@@ -24,6 +41,10 @@ void push(Value value) {
 Value pop() {
 	vm.stackTop--;
 	return *vm.stackTop;
+}
+
+static Value peek(int distance) {
+	return vm.stackTop[-1 - distance];
 }
 
 static InterpretResult run() {
@@ -54,7 +75,13 @@ static InterpretResult run() {
 			case OP_SUBTRACT: BINARY_OP(-); break;
 			case OP_MULTIPLY: BINARY_OP(*); break;
 			case OP_DIVIDE: BINARY_OP(/); break;
-			case OP_NEGATE: push(-pop()); break;
+			case OP_NEGATE: 
+				if (!IS_NUMBER(peek(0))) {
+					runtimeError("Operand must be a number.");
+					return INTERPRET_RUNTIME_ERROR;
+				}
+				push(NUMBER_VAL(-AS_NUMBER(pop())));
+				break;
 			case OP_RETURN: {
 				printValue(pop());
 				printf("\n");
