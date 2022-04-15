9cf53706d9aa3a6670dbb6c9d3ef66885e7a138f
Tomás Almeida <tomas.hugo@gmail.com>
2022-04-14 22:16:26 -0300

    feat: implementado os tipos true, false e nil, além dos operadores lógicos >, <, ==, !, >=, <= !=

--- CLox/vm.c
+++ CLox/vm.c
@@ -1,3 +1,4 @@
+#include <stdarg.h>
 #include <stdio.h>
 
 #include "common.h"
@@ -47,11 +48,15 @@ static Value peek(int distance) {
 	return vm.stackTop[-1 - distance];
 }
 
+static bool isFalsey(Value value) {
+	return IS_NIL(value) || (IS_BOOL(value) && !AS_BOOL(value));
+}
+
 static InterpretResult run() {
 	#define READ_BYTE() (*vm.ip++)
 	#define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
 	// This do while block gives us a way to contain multiple statements inside a block that also permits a semicolon at the end
-	#define BINARY_OP(op) do { double b = pop(); double a = pop(); push(a op b); } while(false)
+	#define BINARY_OP(valueType, op) do { if (!IS_NUMBER(peek(0)) || !IS_NUMBER(peek(1))) { runtimeError("Operands must be numbers."); return INTERPRET_RUNTIME_ERROR; }  double b = AS_NUMBER(pop()); double a = AS_NUMBER(pop()); push(valueType(a op b)); } while(false)
 	for (;;) {
 	#ifdef DEBUG_TRACE_EXECUTION
 		printf("          ");
@@ -71,10 +76,28 @@ static InterpretResult run() {
 				push(constant);
 				break;
 			}
-			case OP_ADD: BINARY_OP(+); break;
-			case OP_SUBTRACT: BINARY_OP(-); break;
-			case OP_MULTIPLY: BINARY_OP(*); break;
-			case OP_DIVIDE: BINARY_OP(/); break;
+			case OP_NIL: push(NIL_VAL); break;
+			case OP_TRUE: push(BOOL_VAL(true)); break;
+			case OP_FALSE: push(BOOL_VAL(false)); break;
+			case OP_EQUAL: {
+				Value a = pop();
+				Value b = pop();
+				push(BOOL_VAL(valuesEqual(a, b)));
+				break;
+			}
+			case OP_GREATER: 
+				BINARY_OP(BOOL_VAL, >);
+				break;
+			case OP_LESS:
+				BINARY_OP(BOOL_VAL, <);
+				break;
+			case OP_ADD: BINARY_OP(NUMBER_VAL, +); break;
+			case OP_SUBTRACT: BINARY_OP(NUMBER_VAL, -); break;
+			case OP_MULTIPLY: BINARY_OP(NUMBER_VAL, *); break;
+			case OP_DIVIDE: BINARY_OP(NUMBER_VAL, /); break;
+			case OP_NOT:
+				push(BOOL_VAL(isFalsey(pop())));
+				break;
 			case OP_NEGATE: 
 				if (!IS_NUMBER(peek(0))) {
 					runtimeError("Operand must be a number.");
