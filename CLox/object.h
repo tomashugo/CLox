c4c40f0818a4e384548ec78f68a98f8a339c0c67
Tomás Almeida <tomas.hugo@gmail.com>
2022-04-15 20:30:21 -0300

    feat: Iniciando a implementação de strings

---
+++ CLox/object.h
@@ -0,0 +1,42 @@
+#ifndef clox_object_h
+#define clox_object_h
+
+#include "common.h"
+#include "value.h"
+
+#define OBJ_TYPE(value)	(AS_OBJ(value)->type)
+
+#define IS_STRING(value) isObjType(value, OBJ_STRING)
+
+#define AS_STRING(value) ((ObjString*)AS_OBJ(value))
+#define AS_CSTRING(value) ((ObjString*)AS_OBJ(value))->chars)
+
+typedef enum {
+	OBJ_STRING,
+} ObjType;
+
+struct Obj {
+	ObjType type;
+};
+
+struct ObjString {
+	Obj obj;
+	int length;
+	char* chars;
+};
+
+/*
+ * Why not just put the body of this function right in the macro? What's
+ * different about this one compared to the others? Right, it’s because the body
+ * uses value twice. A macro is expanded by inserting the argument expression
+ * every place the parameter name appears in the body. If a macro uses a
+ * parameter more than once, that expression gets evaluated multiple times.
+ * 
+ */
+
+static inline bool isObjType(Value value, ObjType type) {
+	return IS_OBJ(value) && AS_OBJ(value)->type == type;
+}
+
+#endif // !clox_object_h
+
