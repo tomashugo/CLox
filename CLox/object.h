a54322109a67afb8ce047cfa740b8a5e37740ae4
Tomás Almeida <tomas.hugo@gmail.com>
2022-04-16 08:54:37 -0300

    feat: Implementado strings e iniciado a implementação do Garbage Collector

--- CLox/object.h
+++ CLox/object.h
@@ -9,7 +9,7 @@
 #define IS_STRING(value) isObjType(value, OBJ_STRING)
 
 #define AS_STRING(value) ((ObjString*)AS_OBJ(value))
-#define AS_CSTRING(value) ((ObjString*)AS_OBJ(value))->chars)
+#define AS_CSTRING(value) (((ObjString*)AS_OBJ(value))->chars)
 
 typedef enum {
 	OBJ_STRING,
@@ -17,6 +17,7 @@ typedef enum {
 
 struct Obj {
 	ObjType type;
+	struct Obj* next;
 };
 
 struct ObjString {
@@ -24,6 +25,10 @@ struct ObjString {
 	int length;
 	char* chars;
 };
+
+ObjString* takeString(char* chars, int length);
+ObjString* copyString(const char* chars, int length);
+void printObject(Value value);
 
 /*
  * Why not just put the body of this function right in the macro? What's
--- CLox/value.c
+++ CLox/value.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
+#include <string.h>
 
+#include "object.h"
 #include "memory.h"
 #include "value.h"
 
@@ -36,15 +38,23 @@ void printValue(Value value) {
 		case VAL_NUMBER:
 			printf("%g", AS_NUMBER(value));
 			break;
+		case VAL_OBJ:
+			printObject(value);
+			break;
 	}
 }
 
 bool valuesEqual(Value a, Value b) {
 	if (a.type != b.type) return false;
 	switch (a.type) {
-	case VAL_BOOL: return AS_BOOL(a) == AS_BOOL(b);
-	case VAL_NIL: return true;
-	case VAL_NUMBER: return AS_NUMBER(a) == AS_NUMBER(b);
-	default: return false;
+		case VAL_BOOL: return AS_BOOL(a) == AS_BOOL(b);
+		case VAL_NIL: return true;
+		case VAL_NUMBER: return AS_NUMBER(a) == AS_NUMBER(b);
+		case VAL_OBJ: {
+			ObjString* aString = AS_STRING(a);
+			ObjString* bString = AS_STRING(b);
+			return aString->length == bString->length && memcmp(aString->chars, bString->chars, aString->length) == 0;
+		}
+		default: return false;
 	}
 }
